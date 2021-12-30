4fa180c6e8a4e848c10f1764e1f57620d4b87c9a
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2021-12-29 19:11:59 -0500

    Fixed indentation

--- neuron/filters/convolve.py
+++ neuron/filters/convolve.py
@@ -5,11 +5,11 @@ from scipy.stats import norm
 def convolve(img, k):
 	"""
 	Parameters: img: ndarray
-                k: size of neighbour
+				k: size of neighbour
     
-    Description: Apply convolve filter. 
+  Description: Apply convolve filter. 
 
-    Returns: ndarray
+  Returns: ndarray
 	"""
 	x = linspace(-k, k, (2*k)+1)
 	kernel = norm.pdf(x)*norm.pdf(x[:,None])
