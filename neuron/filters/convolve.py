43e3936bd51bf4cba89d2a1230e8a570c5e05122
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2021-12-29 19:08:46 -0500

    Added Docstring for convolve implementation

--- neuron/filters/convolve.py
+++ neuron/filters/convolve.py
@@ -4,6 +4,12 @@ from scipy.stats import norm
 
 def convolve(img, k):
 	"""
+	Parameters: img: ndarray
+                k: size of neighbour
+    
+    Description: Apply convolve filter. 
+
+    Returns: ndarray
 	"""
 	x = linspace(-k, k, (2*k)+1)
 	kernel = norm.pdf(x)*norm.pdf(x[:,None])
