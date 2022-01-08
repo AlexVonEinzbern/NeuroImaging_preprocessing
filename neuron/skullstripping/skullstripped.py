35f9806d1c3821de665267708767014ceed498c1
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2022-01-08 11:58:39 -0500

    Fixed indentation

--- neuron/skullstripping/skullstripped.py
+++ neuron/skullstripping/skullstripped.py
@@ -3,9 +3,11 @@ from pyrobex.robex import robex
 def skullstripped(img):
 	"""
 	Parameters: img : nibabel.NiftiImage
+	
 	Description: returns a pyrobex NifTI tuple
-				* stripped : Image of the extracted brain
-				* mask : Image of a binary mask of the brain  
-  Returns: pyrobex.io.NiftiImage
+			* stripped : Image of the extracted brain
+			* mask : Image of a binary mask of the brain  
+  
+	Returns: pyrobex.io.NiftiImage
 	"""
 	return robex(img)
